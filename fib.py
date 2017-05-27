78bc43dd69e61623856e85dd1d8427d1fd383893
JoannaK <joannakoziol@interia.pl>
2017-05-27 16:12:26 +0200

    new refactor

--- fib.py
+++ fib.py
@@ -5,7 +5,9 @@ def sub(a, b):
     return a - b
 
 def add(a, b):
-    return a + b    
+    """Add two numbers"""
+    return a + b
 
-if __name__=='__main__':
+if __name__ == '__main__':
+    print(add.__doc__)
     print('pies')
